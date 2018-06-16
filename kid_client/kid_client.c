7888b27cccd73716f433cad657f9020222df388f
quadhier <quadhier@qq.com>
2018-06-16 15:12:34 +0800

    add url-parsing function to client

--- kid_client/kid_client.c
+++ kid_client/kid_client.c
@@ -9,8 +9,10 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <netdb.h>
 
 #define MULTI_REQ 0
+#define IPSIZE 16
 
 struct conn_arg
 {
@@ -169,9 +171,98 @@ void * put(void *arg)
 	return NULL;
 }
 
+// TODO add mutex to make it thread-safe
+int get_ip_addr(const char *domain, char *ip)
+{
+	struct hostent *phtent;
+	char **paddr = NULL;
+	phtent = gethostbyname(domain);
+	if(phtent == NULL)
+	{
+		perror("get IP address failed");
+		return -1;
+	}
+	for(paddr = phtent->h_addr_list; *paddr != NULL; paddr++)
+	{
+		if(inet_ntop(phtent->h_addrtype, *paddr, ip, IPSIZE))
+		{
+			return 0;
+		}
+	}
+	return -1;
+}
+
+int parse_url(const char *url, char *server_ip, int *server_port, char **rlurl)
+{
+	int urllen = strlen(url);
+	char *rooturl = NULL;
+	char *domain = NULL;
+	const char *slash_end = strchr(url, '/');
+	const char *qmark_end = strchr(url, '?');
+
+	int len = 0;
+	// with slash in url
+	if(slash_end != NULL)
+	{
+		len = slash_end - url;
+		rooturl = (char *)malloc(len + 1);
+		strncpy(rooturl, url, len);
+		rooturl[len] = '\0';
+		*rlurl = (char *)malloc(urllen - len + 1);
+		strcpy(*rlurl, slash_end);
+	}
+	// without slash but with question mark in url
+	else if(qmark_end != NULL)
+	{
+		len = qmark_end - url;
+		rooturl = (char *)malloc(len + 1);
+		strncpy(rooturl, url, len);
+		rooturl[len] = '\0';
+		*rlurl = (char *)malloc(urllen - len + 2);
+		**rlurl = '/';
+		strcpy(*rlurl + 1, qmark_end);
+	}
+	else 
+	{
+		rooturl = (char *)malloc(urllen + 1);
+		strcpy(rooturl, url);
+		*rlurl = (char *)malloc(2);
+		(*rlurl)[0] = '/';
+		(*rlurl)[1] = '\0';
+	}
+	
+	const char *colon_end = strchr(rooturl, ':');
+	if(colon_end != NULL)
+	{
+		*server_port = atoi(colon_end + 1);
+		len = colon_end - rooturl;
+	}
+	else
+	{
+		*server_port = 80;
+		len = strlen(rooturl);
+	}
+
+	domain = (char *)malloc(len + 1);
+	strncpy(domain, rooturl, len);
+	domain[len] = '\0';
+
+	if(get_ip_addr(domain, server_ip) != 0)
+	{
+		free(rooturl);
+		free(domain);
+		return -1;
+	}
+	
+	free(rooturl);
+	free(domain);
+	return 0;
+}
 
 int main(int argc, const char **argv)
 {
+
+
 	if(argc < 3 || argc > 4 || 
 			(strcmp(argv[1], "get") != 0 && strcmp(argv[1], "put") != 0) || 
 			0)
@@ -181,9 +272,21 @@ int main(int argc, const char **argv)
 		exit(EXIT_FAILURE);
 	}
 
+	// parse ip and port of the server
+	char server_ip[IPSIZE];
+	int server_port;
+	char *rlurl = NULL;
+	if(parse_url(argv[2], server_ip, &server_port, &rlurl) != 0)
+	{
+		exit(EXIT_FAILURE);
+	}
+
+//	printf("%s %d %s\n", server_ip, server_port, rlurl);
+//	exit(0);
+
 	// prepare server socket address
-	const char *server_ip = "172.16.181.111";
-	int server_port = 4321;
+//	const char *server_ip = "172.16.181.111";
+//	int server_port = 4321;
 	struct sockaddr_in server_address;
 	bzero(&server_address, sizeof(server_address));
 	server_address.sin_family = AF_INET;
@@ -193,7 +296,7 @@ int main(int argc, const char **argv)
 	if(strcmp(argv[1], "get") == 0)
 	{
 
-		struct conn_arg arg = { (struct sockaddr *)&server_address, argv[2], NULL };
+		struct conn_arg arg = { (struct sockaddr *)&server_address, rlurl, NULL };
 #if MULTI_REQ
 		pthread_t tid;
 		int tnum = 10;
@@ -216,7 +319,8 @@ int main(int argc, const char **argv)
 	else if(strcmp(argv[1], "put") == 0)
 	{
 		const char *body = argc > 3 ? argv[3] : NULL;
-		struct conn_arg arg = { (struct sockaddr *)&server_address, argv[2], body };
+		struct conn_arg arg = { (struct sockaddr *)&server_address, rlurl, body };
 		put(&arg);
 	}
+	free(rlurl);
 }
